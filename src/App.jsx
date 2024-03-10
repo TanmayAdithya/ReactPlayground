74d86ae15cfb3fb12eda848cc70f2481a8ad6f2f
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-03-10 11:38:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
+
 function App() {
+  const [color, setColor] = useState('olive');
+
   return (
-    <>
-      <h1 className="text-3xl font-bold"> Hello world! </h1>
-    </>
+    <div
+      className="bg-gray-600 w-full h-screen flex justify-center"
+      style={{ backgroundColor: color }}
+    >
+      <div className="bg-slate-200 px-1 py-2 fixed bottom-24 rounded-3xl">
+        <button
+          onClick={() => setColor('red')}
+          className="bg-red-300 px-4 py-2 mx-2 rounded-3xl"
+        >
+          Red
+        </button>
+        <button
+          onClick={() => setColor('green')}
+          className="bg-green-300 px-4 py-2 mx-2 rounded-3xl"
+        >
+          Green
+        </button>
+        <button
+          onClick={() => setColor('blue')}
+          className="bg-blue-300 px-4 py-2 mx-2 rounded-3xl"
+        >
+          Blue
+        </button>
+      </div>
+    </div>
   );
 }
 
